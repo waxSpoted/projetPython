9bc0aef30e2ae137712e9752e77d3cdc62a7cd82
wax spoted <wax.herv@gmail.com>
2022-07-15 09:45:07 +0200

    modif script pdf, ajout de la recherche de mot sur pdf en colonne

--- projetPdf/script.py
+++ projetPdf/script.py
@@ -8,7 +8,7 @@ import PyPDF2
 # Function #
 ############
 
-def extractMot(pdf,motRech):
+def extractMotLigne(pdf,motRech):
     pdfFileObj = open(pdf, 'rb')
     pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
     pdfText = ""
@@ -28,12 +28,33 @@ def extractMot(pdf,motRech):
     print("Mot introuvable")
     return False
 
+#################################
+
+def extractMotColonne(pdf,nbCol,motRech):
+    pdfFileObj = open(pdf, 'rb')
+    pdfReader = PyPDF2.PdfFileReader(pdfFileObj)
+    pdfText = ''
+    for page in range(pdfReader.numPages):
+        pageObj = pdfReader.getPage(page)
+        pdfText += pageObj.extractText()
+    textSplit = list(pdfText.split('\n'))
+    for j in range(len(textSplit)):
+        if str(textSplit[j]).strip() == str(motRech).strip() : 
+            print("mot trouvé : ",textSplit[j])
+            print("mot voulu : ",textSplit[j+int(nbCol)+1])
+            print("done")
+            return True
+    print("mot introuvable")
+    return False
+
+##################################"    
+
 def findFile(serialNumber):
     repository="/home/wax/Bureau/informatique/pdfTest/"
     pdfRepo="%s%s.pdf" % (repository,serialNumber)
     return pdfRepo
 
-def findVal (phraseExtraite): 
+def findVal(phraseExtraite): 
     listPhrase=list(phraseExtraite)
     for mot in listPhrase : 
         if mot == "test":
@@ -45,17 +66,44 @@ def findVal (phraseExtraite):
 ########
 
 if __name__ == '__main__': 
+
+# Cas pdf du type 654321.pdf
+  
+    print("Entrer le nom du pdf que vous voulez examiner : ") #testpdf
+    chemin=input()
+    if len(chemin)>0: 
+        print('')
+        print("Entrer le nombre de colonne : ")
+        nbCol=input()
+        if len(nbCol)>0:
+            print('')
+            print("Entrer le mot à rechercher")
+            motRech=input()
+            if len(motRech)>0 : 
+                print('')
+                extractMotColonne(findFile(chemin), nbCol, motRech)
+            else : 
+                print("le mot recherché est invalide ! ")
+                exit
+        else : 
+            print("le nombre de colonne est invalide ! ")
+            exit
+    else : 
+        print("le nom du pdf est invalide veuillez rééssayer ! ")
+    
+# Cas pdf du type 123456.pdf
+    """
     print("Entrer le nom du pdf que vous voulez examiner : ") #testpdf
     chemin=input()
     if len(chemin)>0: 
         print("Entrer le mot à rechercher : ") #Ligne4
         rechercheMot=input()
         if len(rechercheMot)>0:
-            extractMot(findFile(chemin),rechercheMot)
+            extractMotLigne(findFile(chemin),rechercheMot)
         else: 
             print("Aucun mot fourni, veuillez rééssayer ! ")
             exit
     else: 
         print("Le nom du pdf est invalide veuillez rééssayer ! ")
         exit
-     
+    """    
